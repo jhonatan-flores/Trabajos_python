e4d1e25455ac79f0c11f0902b70c7643af64c366
Jhonatan <132001096+jhonatan-flores@users.noreply.github.com>
2023-10-31 11:01:34 -0500

    subiendo trbajo casi terminado 31/10/2023

--- nuevo_semestre/Interfaces_HDS/ejercio_seis.py
+++ nuevo_semestre/Interfaces_HDS/ejercio_seis.py
@@ -1,49 +1,41 @@
 from tkinter import *
 ventana=Tk()
-ventana.title("operaciones aritmeticas")
-ventana.geometry("300x270")
+ventana.title("suma y resta")
+ventana.geometry("500x500")
 
 etiqueta=Label(ventana,text='ingresa un numero: ')
-etiqueta.grid(row="0",column="0")
+etiqueta.pack()
 
 cuadro_texto=Entry()
-cuadro_texto.config(bg="blue",fg="white")
-cuadro_texto.grid()
+cuadro_texto.pack()
 
 etiqueta=Label(ventana,text='ingresa un numero: ')
-etiqueta.grid(row="2",column="0")
+etiqueta.pack()
 
 cuadro_texto=Entry()
-cuadro_texto.config(bg="blue",fg="white")
-cuadro_texto.grid()
+cuadro_texto.pack()
 
 etiqueta=Label(ventana,text='total:')
-etiqueta.grid(row="4",column="0")
+etiqueta.pack()
 
 cuadro_texto=Entry()
-cuadro_texto.config(bg="yellow",fg="blue")
-cuadro_texto.grid()
+cuadro_texto.pack()
 
 
 radiosuma=Radiobutton(ventana,text="sumar",value=0)
-radiosuma.grid(row="1",column="2")
+radiosuma.pack()
 radioresta=Radiobutton(ventana,text="restar",value=1)
-radioresta.grid(row="0",column="2")
+radioresta.pack()
 radiomultiplicar=Radiobutton(ventana,text="multiplicar",value=2)
-radiomultiplicar.grid(row="2",column="2")
+radiomultiplicar.pack()
 radiodividir=Radiobutton(ventana,text="dividir",value=3)
-radiodividir.grid(row="4",column="2")
+radiodividir.pack()
 
-etiqueta=Label(ventana,text='')
-etiqueta.grid(row="7",column="0")
 
 boton=Button(ventana,text="calcular")
-boton.grid
-boton.grid()
-etiqueta=Label(ventana,text='')
-etiqueta.grid(row="9",column="0")
+boton.pack()
 
 boton=Button(ventana,text="limpiar")
-boton.grid()
+boton.pack()
 
 ventana.mainloop()
